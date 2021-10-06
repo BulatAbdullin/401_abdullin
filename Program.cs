3d73b26817f56121b7991addbd1b59f755f8ef94
Bulat Abdullin <bulat@abdullin.xyz>
2021-10-06 21:20:20 +0300

    Classify objects in images in parallel
    Create `ImageClassifier` class, whose constructor takes `imagesDir`
    string and `modelPath` as parameters.
    
    `ProcessDirectoryContentsAsync` method processes files in a directory in
    parallel by creating tasks that return a tuple whose first item is
    file's name and second item is a list of `YoloV4Result` objects.
    `ProcessDirectoryContentsAsync` yield returns tasks' results as tasks
    complete by using `Interleaved` static method.
    
    `Interleaved` static method uses array of buckets where input
    tasks are placed as they complete, one task per bucket.
    
    `Main` is asynchronous. It takes directory name as the first argument
    and writes files' names and identified objects to Console.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using YOLOv4MLNet.DataStructures;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.Error.WriteLine("Usage: %program_name% imageDirectory");
+            Environment.Exit(1);
+        }
+        string dir = args[0];
+        ImageClassifier imageClassifierModel = new ImageClassifier(dir);
+        await foreach (Tuple<string, List<YoloV4Result>> imgRes in
+                       imageClassifierModel.ProcessDirectoryContentsAsync())
+        {
+            Console.WriteLine(imgRes.Item1); // file name
+
+            foreach (YoloV4Result res in imgRes.Item2)
+            {
+                Console.Write("    "); // padding
+                // x1, y1, x2, y2 in page coordinates.
+                // left, top, right, bottom.
+                float x1 = res.BBox[0];
+                float y1 = res.BBox[1];
+                float x2 = res.BBox[2];
+                float y2 = res.BBox[3];
+                Console.WriteLine($"{res.Label}: x1 = {x1}, y1 = {y1}, x2 = {x2}, y2 = {y2}");
+            }
+        }
+    }
+}
